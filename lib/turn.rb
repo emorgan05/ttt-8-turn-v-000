54c74964f659145312746a48fcb5d3d5bdfa9a32
Emily Morgan <emorgan05@gmail.com>
2017-09-02 17:21:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, player_token)
+    move(board, index)
     display_board(board)
   else
     turn(board)
